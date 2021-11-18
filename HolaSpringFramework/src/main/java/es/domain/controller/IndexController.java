fff6d2131885724501227ad46d6d38b724a4fd40
Erika <erikaroxana24@gmail.com>
2021-11-18 18:20:41 +0100

    Práctica 1 con Spring Framework

---
+++ HolaSpringFramework/src/main/java/es/domain/controller/IndexController.java
@@ -0,0 +1,43 @@
+package es.domain.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController{
+	
+	@RequestMapping("/home")
+	public String goIndex() {
+		return "Index";
+	}
+	
+	@RequestMapping("/")
+	public String getPresentacion() {
+		return "Presentacion";
+	}
+	
+	@RequestMapping("/listado")
+	public String goListado(Model model) {
+		List<String> alumnos = new ArrayList<String>();
+		alumnos.add("Gabriela");
+		alumnos.add("Pedro");
+		alumnos.add("Harry");
+		alumnos.add("Lorena");
+		
+		model.addAttribute("titulo", "Listado de alumnos");
+		model.addAttribute("profesor", "Gabriel Casas");
+		model.addAttribute("alumnos", alumnos);
+			
+		return "Listado";
+	}
+	
+	
+	
+	
+	
+	
+}
